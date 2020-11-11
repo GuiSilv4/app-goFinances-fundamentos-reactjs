9ff575f417b3c90f18c87dde65491e2a3cd40d38
Guilherme da Silva <guisilva.dev@gmail.com>
2020-11-11 22:41:32 +0000

    upload drag and drop added

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -17,8 +17,6 @@ interface Transaction {
   id: string;
   title: string;
   value: number;
-  formattedValue: string;
-  formattedDate: string;
   type: 'income' | 'outcome';
   category: { title: string };
   created_at: string;
